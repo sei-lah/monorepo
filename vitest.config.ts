0a33f37dabe2408c858bfd355a0c7191b62aa7f9
Mike Valeriano <mkvlrn@gmail.com>
2026-01-16 18:24:04 -0300

    chore: update base vitest config to use workspace

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,24 +1,12 @@
 import { defineConfig } from "vitest/config";
 
-export default defineConfig(() => {
-  return {
-    test: {
-      include: ["**/*.{test,spec}.ts"],
-      exclude: ["node_modules"],
-      reporters: ["tree"],
-      watch: false,
-      coverage: {
-        all: true,
-        clean: true,
-        cleanOnRerun: true,
-        include: ["src"],
-        exclude: ["**/*.{test,spec}.ts", "src/main.ts"],
-      },
-      // biome-ignore lint/style/useNamingConvention: needed for vitest
-      env: { NODE_ENV: "test" },
-      environment: "node",
-      passWithNoTests: true,
-      setupFiles: [],
+export default defineConfig({
+  test: {
+    projects: ["apps/*", "pkg/*"],
+    reporters: ["tree"],
+    watch: false,
+    coverage: {
+      exclude: ["**/*.{test,spec}.ts", "**/*.config.ts"],
     },
-  };
+  },
 });
