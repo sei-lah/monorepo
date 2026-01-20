954e69fb7c0baf07819f6de54e19f69379f193cd
Mike Valeriano <mkvlrn@gmail.com>
2026-01-20 17:01:45 -0300

    feat: add varlock, pg, drizzle to api - configure first migration

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,13 +1,4 @@
-/** biome-ignore-all lint/suspicious/noConsole: needed, yo */
+import "varlock/auto-load";
+import { ENV } from "varlock";
 
-import { divide } from "#math/advanced";
-import { add } from "#math/basic";
-
-const sum = add(1, 2);
-console.log(`1 + 2 = ${sum}`);
-const div = divide(4, 2);
-if (div.isError) {
-  console.error(div.error.message);
-} else {
-  console.log(`4 / 2 = ${div.value}`);
-}
+console.log(ENV.DATABASE_URL);
