a65292a83be7260883497d33235b51b14b08582f
Daniel Billé <danielbilleds@gmail.com>
2026-02-03 10:46:58 -0300

    feat: adicinado as variacoes das expressoes de rolamento de dados

--- pkg/utils/src/dice/image-generation.ts
+++ pkg/utils/src/dice/image-generation.ts
@@ -4,17 +4,17 @@ import type { RollResult } from "#dice/roll";
 const boxSize = 80;
 const pad = 10;
 
-function square(ctx: SKRSContext2D, x: number, y: number, value: string) {
-  ctx.fillStyle = "#330066";
+function square(ctx: SKRSContext2D, x: number, y: number, value: string, color: string) {
+  ctx.fillStyle = color;
   ctx.beginPath();
   ctx.roundRect(x, y, boxSize, boxSize, 10);
   ctx.fill();
 
-  ctx.strokeStyle = "#6666FF";
+  ctx.strokeStyle = "#ffffff";
   ctx.lineWidth = 3;
   ctx.stroke();
 
-  ctx.fillStyle = "#afafff";
+  ctx.fillStyle = "#ffffff";
   ctx.font = "bold 40px sans-serif";
   ctx.textAlign = "center";
   ctx.textBaseline = "middle";
@@ -27,11 +27,11 @@ function circle(ctx: SKRSContext2D, x: number, y: number, value: string) {
   ctx.arc(x + boxSize / 2, y + boxSize / 2, boxSize / 2, 0, Math.PI * 2);
   ctx.fill();
 
-  ctx.strokeStyle = "#007acc";
+  ctx.strokeStyle = "#ffffff";
   ctx.lineWidth = 3;
   ctx.stroke();
 
-  ctx.fillStyle = "#4fb8ff";
+  ctx.fillStyle = "#ffffff";
   ctx.font = "bold 40px sans-serif";
   ctx.textAlign = "center";
   ctx.textBaseline = "middle";
@@ -39,7 +39,11 @@ function circle(ctx: SKRSContext2D, x: number, y: number, value: string) {
 }
 
 export function imgGenerator(result: RollResult): Buffer {
-  const width = result.values.length * (boxSize + pad * 2) + pad * 2 + boxSize;
+  const variacao = !result.variation;
+
+  const qntDados = result.values.length + (variacao ? 1 : 2);
+
+  const width = qntDados * (boxSize + pad * 2);
   const height = boxSize + pad * 2;
 
   const canvas = createCanvas(width, height);
@@ -47,9 +51,22 @@ export function imgGenerator(result: RollResult): Buffer {
 
   result.values.forEach((value, i) => {
     const x = i * (boxSize + pad) + pad;
-    square(ctx, x, pad, value.toString());
+    square(ctx, x, pad, value.toString(), "#330066");
   });
 
+  if (result.variation < 0) {
+    const x = result.values.length * (boxSize + pad * 2);
+    square(ctx, x, pad, result.variation.toString(), "#810000");
+  } else if (result.variation > 0) {
+    const x = result.values.length * (boxSize + pad * 2);
+    square(ctx, x, pad, result.variation.toString(), "#009719");
+  }
+
+  if (result.variation) {
+    const x = (result.values.length + 1) * (boxSize + pad * 2);
+    circle(ctx, x, pad, result.total.toString());
+    return canvas.toBuffer("image/png");
+  }
   const x = result.values.length * (boxSize + pad * 2);
   circle(ctx, x, pad, result.total.toString());
 
--- pkg/utils/src/dice/roll.ts
+++ pkg/utils/src/dice/roll.ts
@@ -2,6 +2,7 @@ export interface RollResult {
   qnt: number;
   face: number;
   values: number[];
+  variation: number;
   total: number;
 }
 
@@ -10,17 +11,23 @@ export const maxDice = 10;
 export const diceFaces = [3, 4, 6, 8, 10, 12, 20];
 
 export function roll(xp: string): RollResult | false {
-  const regex = new RegExp(`^(${maxDice}|[${minDice}-${maxDice - 1}])d(${diceFaces.join("|")})$`);
+  const regex = new RegExp(
+    `^(${maxDice}|[${minDice}-${maxDice - 1}])d(${diceFaces.join("|")})([+-]\\d+)?$`,
+  );
   const match = xp.match(regex);
   if (!match) {
     return false;
   }
   const qnt = Number(match[1]);
   const face = Number(match[2]);
+  const variation = Number(match[3]);
   const values = Array.from({ length: qnt }, () => Math.floor(Math.random() * face) + 1);
   let total = 0;
   for (const value of values) {
     total += value;
   }
-  return { qnt, face, values, total };
+  if (variation) {
+    total += variation;
+  }
+  return { qnt, face, values, variation, total };
 }
